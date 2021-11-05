2bd274362a7c03a9fc4e1ce78e33f4f4291034da
mashazhivitsa <maho.miname@gmail.com>
2021-11-05 11:51:16 +0300

    refactor: add an 'exit' case

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,34 +1,26 @@
-const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
-const filePath = path.join(__dirname, 'text.txt');
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+const filePath = path.join(__dirname, "text.txt");
 
 let writeableStream = fs.createWriteStream(filePath);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const rl = readline.createInterface({ input : process.stdin, 
-                                     output : process.stdout
-                                    });
-
-
-rl.question(`Please enter your text line: \n`, (firstInput) => {
-    if (firstInput.toUpperCase() === 'EXIT') {
-        rl.close(); 
+const saveMsgInToFile = (userInput) => {
+    if (userInput.toUpperCase() === 'EXIT') {
+        rl.close();
     } else {
-        writeableStream.write(`${firstInput} \n`);
+        writeableStream.write(`${userInput} \n`)
     }
+};
 
-    rl.on('line', (userInput) => {
-        if (userInput.toUpperCase() === 'EXIT') {
-            rl.close();
-        } else {
-            console.log(userInput);
-            writeableStream.write(`${userInput} \n`);
-        }
-      });
-})
+console.log(`Please enter your text line: \n`);
 
-rl.on('close', () => {
-    console.log('Your text has been successfully written to the file!')
-})
+rl.on("line", saveMsgInToFile);
 
-
+rl.on("close", () => {
+  console.log("Your text has been successfully written to the file!");
+});
